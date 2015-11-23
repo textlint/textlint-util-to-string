2a9eead2fd98c8a8483513988d31bd23d60671ed
azu <azuciao@gmail.com>
2015-11-23 16:52:33 +0900

    feat: re initialize

--- src/to-string.js
+++ src/to-string.js
@@ -1,59 +1,120 @@
 // LICENSE : MIT
 "use strict";
-class Token {
-    constructor() {
-
-    }
-}
+import ObjectAssign from "object-assign";
 export default class Stringify {
     constructor(node) {
         this.node = node;
-        this.nodeMap = {};
+        this.tokenMaps = [];
+        this.generatedString = "";
+        // pre calculate
+        this.stringify(this.node);
         /*
-        start: {
-        // start with node.range relative
-            generated : [start, end]
+        [
+        // e.g.) **Str**
+        {
+            // original range
+            // e.g.) [0, 7] = `**Str**`
             original : [start, end]
-        }
+            // trim value from Original = intermediate
+            // e.g.) [2, 5]
+            intermediate: [start, end]
+            // actual value = Str
+            // e.g.) [0, 3]
+            generated : [start, end]
+        }]
          */
     }
 
-    revert(position) {
-
+    originalPositionFor(position) {
+        let hitTokenMaps = this.tokenMaps.filter(tokenMap => {
+            let generated = tokenMap.generated;
+            if (generated[0] <= position && position <= generated[1]) {
+                return true;
+            }
+        });
+        if (hitTokenMaps.length === 0) {
+            return;
+        }
+        // a bcd
+        // b = index 1
+        // original `a` bcd
+        // originalRange [3, 7]
+        // adjustedStart = 1
+        // b's index = 3 + 1
+        let hitTokenMap = hitTokenMaps[0];
+        let adjustedStart = position - hitTokenMap.generated[0];
+        return hitTokenMap.original[0] + adjustedStart;
     }
 
-    valueOf(node, parent) {
+    _getValue(node) {
+        if (node.value) {
+            return node.value;
+        } else if (node.alt) {
+            return node.alt;
+        } else if (node.title) {
+            return node.title;
+        }
+    }
+
+    _valueOf(node, parent) {
         if (!node) {
             return;
         }
+
+
         // [padding][value][padding]
         // =>
         // [value][value][value]
-        var value = (node.value ? node.value :
-                     (node.alt ? node.alt : node.title)) || '';
-        if (parent == null) {
-            return value;
-        }
-        console.log(node.type + "=>" + value);
-        if (value.length === 0) {
+        let value = this._getValue(node);
+        if (!value) {
             return;
         }
-        let rawValue = parent.raw;
-        console.log(rawValue);
-        let paddingLeft = rawValue.indexOf(value);
+        if (parent == null) {
+            return;
+        }
+        // <p><Str /></p>
+        if (parent.type === "Paragraph" && node.type === "Str") {
+            return {
+                original: node.range,
+                intermediate: node.range,
+                value: value
+            };
+        }
+        // <p><code>code</code></p>
+        // => container is <p>
+        // <p><strong><Str /></strong></p>
+        // => container is <strong>
+        let container = (parent.type === "Paragraph") ? node : parent;
+        let rawValue = container.raw;
+        let paddingLeft = rawValue.indexOf(value, 1); // avoid match ! with ![
         let paddingRight = rawValue.length - (paddingLeft + value.length);
-
-        let originalRange = [
-            this.node.range[0] - parent.range[0],
-            this.node.range[0] - parent.range[0] + parent.range[1]
-        ];
-        let generatedRange = [
+        let originalRange = container.range;
+        let intermediateRange = [
             originalRange[0] + paddingLeft,
             originalRange[1] - paddingRight
         ];
-        console.log("padding", paddingLeft, paddingRight);
-        console.log("G", generatedRange, originalRange);
-        return value;
+        return {
+            original: originalRange,
+            intermediate: intermediateRange,
+            value: value
+        };
+
+    }
+
+    _addTokenMap(tokenMap) {
+        if (tokenMap == null) {
+            return;
+        }
+        let addedTokenMap = ObjectAssign({}, tokenMap);
+        if (this.tokenMaps.length === 0) {
+            let textLength = addedTokenMap.intermediate[1] - addedTokenMap.intermediate[0];
+            addedTokenMap["generated"] = [0, textLength];
+        } else {
+            let textLength = addedTokenMap.intermediate[1] - addedTokenMap.intermediate[0];
+            addedTokenMap["generated"] = [this.generatedString.length, this.generatedString.length + textLength];
+        }
+        this.generatedString += tokenMap.value;
+        this.tokenMaps.push(addedTokenMap);
     }
 
     /**
@@ -62,16 +123,24 @@ export default class Stringify {
      * recursivly try its children.
      *
      * @param {Node} node - Node to transform to a string.
+     * @param {Node} parent - Parent Node of the `node`.
      * @return {string} - Textual representation.
      */
-    toString(node, parent) {
-        let value = this.valueOf(node, parent);
+    stringify(node, parent) {
+        let value = this._valueOf(node, parent);
         if (value) {
             return value;
         }
-        return (node.children && node.children.map((childNode) => {
-                let parentOfChildNode = node;
-                return this.toString(childNode, parentOfChildNode);
-            }).join('')) || '';
+        if (!node.children) {
+            return;
+        }
+        node.children.forEach((childNode) => {
+            let tokenMap = this.stringify(childNode, node);
+            this._addTokenMap(tokenMap);
+        });
+    }
+
+    toString() {
+        return this.generatedString;
     }
 }
--- test/to-string-test.js
+++ test/to-string-test.js
@@ -4,9 +4,154 @@ import assert from "power-assert"
 import {parse} from "markdown-to-ast";
 import Stringify from "../src/to-string";
 describe("to-string-test", function () {
-    it("should", function () {
-        let AST = parse("**str**");
-        let st = new Stringify(AST);
-        st.toString(AST);
+    context("Each Pattern", function () {
+        it("Str", function () {
+            let AST = parse("**str**");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "str");
+            assert.equal(source.tokenMaps.length, 1);
+            let token = source.tokenMaps[0];
+            assert.deepEqual(token, {
+                generated: [0, 3],
+                intermediate: [2, 5],
+                original: [0, 7],
+                value: "str"
+            });
+        });
+        it("Str that contain break line", function () {
+            let AST = parse("**st\nr**");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "st\nr");
+            assert.equal(source.tokenMaps.length, 1);
+            let token = source.tokenMaps[0];
+            assert.deepEqual(token, {
+                generated: [0, 4],
+                intermediate: [2, 6],
+                original: [0, 8],
+                value: "st\nr"
+            });
+        });
+        it("Link", function () {
+            let AST = parse("_[link](http://example)");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "_link");
+            assert.equal(source.tokenMaps.length, 2);
+            let tokenStr = source.tokenMaps[0];
+            assert.deepEqual(tokenStr, {
+                generated: [0, 1],
+                intermediate: [0, 1],
+                original: [0, 1],
+                value: "_"
+            });
+            let tokenLink = source.tokenMaps[1];
+            assert.deepEqual(tokenLink, {
+                generated: [1, 5],
+                intermediate: [2, 6],
+                original: [1, 23],
+                value: "link"
+            });
+        });
+        it("Str + `Code` + Str", function () {
+            let AST = parse("text`code`text");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "textcodetext");
+            assert.equal(source.tokenMaps.length, 3);
+            assert.deepEqual(source.tokenMaps[0], {
+                generated: [0, 4],
+                intermediate: [0, 4],
+                original: [0, 4],
+                value: "text"
+            });
+            assert.deepEqual(source.tokenMaps[1], {
+                generated: [4, 8],
+                intermediate: [5, 9],
+                original: [4, 10],
+                value: "code"
+            });
+            assert.deepEqual(source.tokenMaps[2], {
+                generated: [8, 12],
+                intermediate: [10, 14],
+                original: [10, 14],
+                value: "text"
+            });
+        });
+        it("Header", function () {
+            let AST = parse("# Header");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "Header");
+            assert.equal(source.tokenMaps.length, 1);
+            assert.deepEqual(source.tokenMaps[0], {
+                generated: [0, 6],
+                intermediate: [2, 8],
+                original: [0, 8],
+                value: "Header"
+            });
+        });
+        it("Image + Str", function () {
+            let AST = parse("![alt](http://example.png) text");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "alt text");
+            assert.equal(source.tokenMaps.length, 2);
+            assert.deepEqual(source.tokenMaps[0], {
+                generated: [0, 3],
+                intermediate: [2, 5],
+                original: [0, 26],
+                value: "alt"
+            });
+            assert.deepEqual(source.tokenMaps[1], {
+                generated: [3, 8],
+                intermediate: [26, 31],
+                original: [26, 31],
+                value: " text"
+            });
+        });
+        it("confuse pattern", function () {
+            let AST = parse("![!](http://example.com)");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "!");
+            assert.equal(source.tokenMaps.length, 1);
+            assert.deepEqual(source.tokenMaps[0], {
+                generated: [0, 1],
+                intermediate: [2, 3],
+                original: [0, 24],
+                value: "!"
+            });
+        });
+        it("Empty", function () {
+            let AST = parse("");
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "");
+            assert.equal(source.tokenMaps.length, 0);
+        });
     });
+
+    describe("#originalPositionFor", function () {
+        it("should return original position for index", function () {
+            var originalText = "![alt](http://example.png) text";
+            let AST = parse(originalText);
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "alt text");
+            var indexOf = result.indexOf("text");
+            assert.equal(indexOf, 4);
+            assert.deepEqual(source.originalPositionFor(indexOf), 27);
+            assert.equal(originalText.slice(27), "text");
+        });
+        it("should return null when not found position for index", function () {
+            var originalText = "![alt](http://example.png) text";
+            let AST = parse(originalText);
+            let source = new Stringify(AST);
+            let result = source.toString();
+            assert.equal(result, "alt text");
+            assert.equal(source.originalPositionFor(1000), null);
+        });
+    })
 });
